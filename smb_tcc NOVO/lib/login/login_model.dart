37757e60fffbb219d0e30a69722881519225f45f
Bela357 <isabelamendesg20@gmail.com>
2024-10-31 12:05:17 -0300

    apagar login

--- smb_tcc NOVO/lib/login/login_model.dart
+++ smb_tcc NOVO/lib/login/login_model.dart
@@ -8,55 +8,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';  // Para trabalhar com JSON
-///HAHAHA
 
-class LoginModel extends FlutterFlowModel<LoginWidget> {
-  ///  State fields for stateful widgets in this page.
-  ///  /// HAHAHAHAA
-  ///
-  // State field(s) for user widget.
-  FocusNode? userFocusNode;
-  TextEditingController? userTextController;
-  String? Function(BuildContext, String?)? userTextControllerValidator;
 
-  @override
-  void initState(BuildContext context) {}
-
-  @override
-  void dispose() {
-    userFocusNode?.dispose();
-    userTextController?.dispose();
-  }
-
-  Future<void> login(String email) async {
-    var url = Uri.parse('http://localhost/smb_tcc/login.php');
-    var dados = {'email': email};
-
-    try {
-      var response = await http.post(url, body: dados);
-
-      if (response.statusCode == 200) {
-        var jsonResponse = json.decode(response.body);
-
-        if (jsonResponse['status'] == 'sucesso') {
-          // Se o email for encontrado, vá para a página de criar senha
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => LOGINcriarsenha()),
-          );
-        } else {
-          // Se o email não for encontrado, vá para o site
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => PaginaSITEScreen()),
-          );
-        }
-      } else {
-        print('Erro na requisição: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Erro: $e');
-    }
-  }
 
 }
